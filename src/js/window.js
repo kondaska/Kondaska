3efed72e698e3fae1c4ef8ae3899b999b77c700c
Thijs Boom <twboom@hotmail.nl>
2021-03-03 19:40:57 +0100

    Working on creating windows and some CSS

---
+++ src/js/window.js
@@ -0,0 +1,17 @@
+let view = []
+
+class View {
+    constructor(name, content, rescalable, width, height, id, classes) {
+        this.name = name;
+        this.content = content;
+        this.rescalable = rescalable;
+        this.width = width;
+        this.height = height;
+        this.id = id;
+        this.classes = classes;
+    };
+
+    show() {
+
+    }
+}
