6f05f0c3ae05cfb721857515c011c6c1e849cdd2
Thijs Boom <twboom@hotmail.nl>
2021-03-04 16:31:19 +0100

    Working on windows and basics in general

--- src/js/core.js
+++ src/js/core.js
@@ -1,2 +1,33 @@
 let kondaska = [];
 let system = [];
+
+kondaska.protoFiles = [];
+kondaska.protoFiles.html = [
+    'assets/prototypes/window.html'
+]
+
+kondaska.files = {
+    '': '',
+}
+
+kondaska.load = {};
+kondaska.load.protoFiles = function() {
+    for (object in kondaska.protoFiles) {
+        const array = kondaska.protoFiles[object]
+        kondaska.files[object] = ''
+        const target = kondaska.files[object]
+        console.log(target)
+        for (let i = 0; i < array.length; i++) {
+            const file = array[i]
+            console.log(file)
+            const fileName = file.replace('.','/').split('/')
+            const fetchFile = fileName[fileName.length - 2]
+            console.log(fetchFile)
+            fetch(file)
+                .then(response => response.text())
+                .then(data => target[fetchFile] = data)
+        }
+    }
+}
+
+kondaska.load.protoFiles()
--- src/js/window.js
+++ src/js/window.js
@@ -1,17 +1,37 @@
 let view = []
 
 class View {
-    constructor(name, content, rescalable, width, height, id, classes) {
+    constructor(name, title, content, classes) {
         this.name = name;
+        this.title = title;
         this.content = content;
-        this.rescalable = rescalable;
-        this.width = width;
-        this.height = height;
-        this.id = id;
         this.classes = classes;
-    };
+    }
 
-    show() {
+    create() {
+        const newWindow = document.createElement('div');
+        newWindow.setAttribute('class', 'window');
+        newWindow.setAttribute('class', this.name)
+        newWindow.innerHTML = content;
+        if (this.classes !== undefined) {
+            for (let i = 0; i < this.classes.length; i++) {
+                newWindow.setAttribute('class', this.classes[i])
+            };
+        };
+        document.querySelector('#desktop').appendChild(newWindow);
+    }
+
+    close() {
 
     }
 }
+
+/*
+view.open = function(name, content) {
+    const newWindow = document.createElement('div')
+    newWindow.setAttribute('class', 'window')
+    newWindow.setAttribute('class', name)
+    newWindow.innerHTML = content;
+    document.querySelector('#desktop').appendChild(newWindow)
+}
+*/
