f72939635396d23d49c9fd3b2e4fda043c76123d
Thijs Boom <twboom@hotmail.nl>
2021-03-04 17:53:04 +0100

    Finished protofile fetch system

--- src/js/core.js
+++ src/js/core.js
@@ -22,15 +22,12 @@ kondaska.load = {};
 kondaska.load.protoFiles = function() {
     for (object in kondaska.protoFiles) {
         const array = kondaska.protoFiles[object]
-        kondaska.files[object] = ''
+        kondaska.files[object] = {}
         const target = kondaska.files[object]
-        console.log(target)
         for (let i = 0; i < array.length; i++) {
             const file = array[i]
-            console.log(file)
             const fileName = file.replace('.','/').split('/')
             const fetchFile = fileName[fileName.length - 2]
-            console.log(fetchFile)
             fetch(file)
                 .then(response => response.text())
                 .then(data => target[fetchFile] = data)
