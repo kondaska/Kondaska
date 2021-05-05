79161f95baa0b2a96e3f892840752980f1c1bb1f
Thijs Boom <twboom@hotmail.nl>
2021-05-05 16:51:53 +0200

    Made the basics of the shell functions

--- js/kondaska.js
+++ js/kondaska.js
@@ -1,1 +1,17 @@
-const kondaska = [];
+/* Kondaska Shell */
+const shell = [];
+
+// Shell log
+shell.log = function(msg) {
+    console.log(msg)
+}
+
+// Shell warn
+shell.warn = function(msg) {
+    console.error(msg)
+}
+
+// Shell error
+shell.error = function(msg) {
+    console.error(msg)
+}
