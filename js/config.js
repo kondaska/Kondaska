4f581bab1f85809f79340f08cbc10a2af9a1ef0c
Thijs Boom <twboom@hotmail.nl>
2021-05-05 17:15:09 +0200

    Started work on windows

--- js/config.js
+++ js/config.js
@@ -1,1 +1,10 @@
+/* Configuration */
 const config = [];
+
+
+/* Defaults */
+const defaults = [];
+
+defaults.viewWidth = 50;
+defaults.viewHeight = 50;
+defaults.viewResize = true;
--- js/kondaska.js
+++ js/kondaska.js
@@ -1,3 +1,8 @@
+/* Generic */
+const system = [];
+
+system.views = [];
+
 /* Kondaska Shell */
 const shell = [];
 
@@ -15,3 +20,37 @@ shell.warn = function(msg) {
 shell.error = function(msg) {
     console.error(msg)
 }
+
+/* Views (window) */
+class View {
+    constructor(name, app, content, properties) {
+
+        if (typeof properties !== 'object') { throw new Error(`Properties is not an object`) };
+
+        this.name = name;
+        this.app = app;
+        this.content = content;
+        this.resizable = properties.resizable;
+        this.width = properties.width;
+        this.height = properties.height;
+        this.data = properties.data;
+
+        if (typeof this.resizable !== "boolean") { this.resizable = defaults.viewResize; shell.warn(`Property "resizeable" was not defined or was not a boolean`, 'viewhandler') };
+        if (typeof this.width !== "interger") { this.width = defaults.viewWidth; shell.warn(`Property "width" was not defined or was not an interger`, 'viewhandler') };
+        if (typeof this.height !== "interger") { this.width = defaults.viewHeight; shell.warn(`Property "height" was not defined or was not an interger`, 'viewhandler') };;
+
+    };
+
+    create() {
+        // Creating the window itself
+
+        // Container window
+        const container = document.createElement('div');
+        container.classList = 'view container'
+
+        // View header
+        const header = document.createElement('div');
+        header.classList = 'view header';
+        
+    }
+};
