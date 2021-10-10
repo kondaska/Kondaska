fe7e2ed46ab5f84ea5bce74a17d8344b12bf29da
Thijs Boom <twboom@hotmail.nl>
2021-10-10 22:10:36 +0200

    Started working on apps for real

--- js/kondaska.js
+++ js/kondaska.js
@@ -291,20 +291,53 @@ const TaskButton = class {
     };
 };
 
-
 /* Apps */
 class App {
 
-    constructor(name, icon, files, ) {
+    properties = {
+        name: 'MyApp',
+        version: '1.0.0',
+        description: 'This is my App!',
+    };
 
-        this.name = name;
-        this.icon = icon;
-        this.files
+    sources = {
+        js: [],
+        css: [],
+        icon: '',
+    };
 
-    }
+    constructor(properties, sources) {
 
-}
+        if (properties !== undefined) { this.properties = {...this.properties, ...properties} };
+        if (sources !== undefined) { this.sources = {...this.sources, ...sources} };
 
+    };
+
+    start = function () {
+        shell.error(`Tried to start ${this.properties.name}, but no start function defined!`);
+    };
+
+    load() {
+        const js = this.sources.js;
+        const css = this.sources.css;
+
+        // Load JS
+        js.forEach(file => {
+            const el = document.createElement('script');
+            el.src = file;
+            document.head.appendChild(el);
+        });
+
+        // Load CSS
+        css.forEach(file => {
+            const el = document.createElement('link');
+            el.rel = 'stylesheet';
+            el.href = file;
+            document.head.appendChild(el);
+        });
+    };
+
+};
 
 /* Boot / Initiation */
 
